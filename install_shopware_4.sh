91d8bd9ed70e2de5c15abdca6a708fec78a13155
nasdoAG-fstellmann <f.stellmann@nasdo.de>
2022-06-07 19:59:54 +0200

    Update install_shopware_4.sh

--- install_shopware_4.sh
+++ install_shopware_4.sh
@@ -2,9 +2,9 @@
 echo "Press any key to install shopware"
 read -t 3 -n 1
 
-wget https://www.shopware.com/de/Download/redirect/version/sw6/file/install_v6.4.11.1_4acac2b5012d6a03377ae5881590aec8cda0196b.zip
-unzip install_v6.4.11.1_4acac2b5012d6a03377ae5881590aec8cda0196b.zip
-composer install
+sudo wget https://www.shopware.com/de/Download/redirect/version/sw6/file/install_v6.4.11.1_4acac2b5012d6a03377ae5881590aec8cda0196b.zip
+sudo unzip install_v6.4.11.1_4acac2b5012d6a03377ae5881590aec8cda0196b.zip
+sudo composer install
 
 sudo chown -R www-data:www-data /var/www/html/shopware
 sudo chmod -R 755 /var/www/html/shopware

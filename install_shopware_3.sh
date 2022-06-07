a7eef17e2dfc5df579808b1c4769dfde3356888a
nasdoAG-fstellmann <f.stellmann@nasdo.de>
2022-06-07 19:43:16 +0200

    Update install_shopware_3.sh

--- install_shopware_3.sh
+++ install_shopware_3.sh
@@ -6,7 +6,10 @@ sudo chmod 777 /usr/bin/
 sudo curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
 
 sudo mkdir /var/www/html/shopware
-sudo cd /var/www/html/shopware && wget https://www.shopware.com/de/Download/redirect/version/sw6/file/install_v6.4.11.1_4acac2b5012d6a03377ae5881590aec8cda0196b.zip && unzip install_v6.4.11.1_4acac2b5012d6a03377ae5881590aec8cda0196b.zip && composer install
+cd /var/www/html/shopware
+sudo wget https://www.shopware.com/de/Download/redirect/version/sw6/file/install_v6.4.11.1_4acac2b5012d6a03377ae5881590aec8cda0196b.zip
+sudo unzip install_v6.4.11.1_4acac2b5012d6a03377ae5881590aec8cda0196b.zip
+sudo composer install
 
 sudo chown -R www-data:www-data /var/www/html/shopware
 sudo chmod -R 755 /var/www/html/shopware
